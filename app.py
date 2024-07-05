584a33a627f36df49119d9c7a42f31ca0d8cee64
Daniel Fromm <88262637+DFromm50818@users.noreply.github.com>
2024-07-05 13:11:00 +0000

    new feature added

--- app.py
+++ app.py
@@ -1,23 +1,48 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, g
 import os
+from database import db, Todo
+
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-todo_file = os.path.join(basedir, "todo_list.txt")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "todos.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 todo_list = []
 
-# Load te to-do list from a file
-try:
-    with open(todo_file, "r") as file:
-        for line in file:
-            todo_list.append(line.strip())
-except FileNotFoundError:
-    pass
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+@app.before_request
+def load_data_to_g():
+    todos = Todo.query.all()
+    g.todos = todos
+    g.todo = None
 
 @app.route("/")
 def index():
-    return render_template("index.html", todo_list=todo_list)
+    return render_template("index.html")
+
+@app.route("/add", methods=["POST"])
+def add_todo():
+    # get the data from the form
+    todo = Todo(
+        name=request.form["todo"],
+    )
+
+    # add the new ToDo to the list
+    db.session.add(todo)
+    db.session.commit()
+    
+    # add the new ToDo to the list
+    return redirect(url_for("index"))
+
+@app.route("/remove/<int:id>", methods=["GET", "POST"])
+def remove_todo(id):
+    db.session.delete(Todo.query.filter_by(id=id).first())
+    db.session.commit()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
