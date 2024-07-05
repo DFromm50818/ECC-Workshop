496613d178b8eeda73134413f5e2fb15d30df63f
Daniel Fromm <88262637+DFromm50818@users.noreply.github.com>
2024-07-05 12:01:39 +0000

    First upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+todo_file = os.path.join(basedir, "todo_list.txt")
+
+todo_list = []
+
+# Load te to-do list from a file
+try:
+    with open(todo_file, "r") as file:
+        for line in file:
+            todo_list.append(line.strip())
+except FileNotFoundError:
+    pass
+
+@app.route("/")
+def index():
+    return render_template("index.html", todo_list=todo_list)
+
+if __name__ == "__main__":
+    app.run(debug=True)
